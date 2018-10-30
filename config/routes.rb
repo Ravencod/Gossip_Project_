26f0978dcea72a224410e75ade28d2ef86a03530
Ravencod <caillauxr@gmail.com>
2018-10-30 13:19:31 +0100

    link_to modif

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'gossip/:index', to: 'gossip#gossip', as: 'gossip'
-  get '/', to: 'home#index'
   get '/team', to: 'team#index'
   get '/contact', to: 'contact#index'
 end
